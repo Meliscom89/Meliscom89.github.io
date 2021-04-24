83b3839f12531a93ca959839b0c986a2ba201015
Meliscom89 <Meliscom@aol.com>
2021-04-24 00:47:54 -0400

    last update

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,10 @@
-
 let d = new Date();
-
 
 let year = d.getFullYear();
 
 document.querySelector("#currentYear").textContent = year;
 
+
+let update = document.lastModified;
+
+document.querySelector("#lastUpdate").textContent = update;
