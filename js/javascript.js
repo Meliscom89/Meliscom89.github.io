a2beb72b0e4808b1840c06c2eb5821f0e5ebf0e6
Meliscom89 <Meliscom@aol.com>
2021-04-24 00:39:41 -0400

    date update

--- js/javascript.js
+++ js/javascript.js
@@ -4,28 +4,5 @@ let d = new Date();
 
 let year = d.getFullYear();
 
-year = document.querySelector(`#currentYear`);
+document.querySelector("#currentYear").textContent = year;
 
-/*let dayNames = [
-    "Sunday",
-    "Monday",
-    "Tuesday".
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-]
-let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-]*/
